dccef8e8573a93f5f850adc79447c507ce8a9851
fuouo <fuouokun@gmail.com>
2017-10-22 21:48:22 +0800

    Added Rods and CraneHandler

---
+++ Assets/Scripts/BallDropMachine/CraneHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneHandler : MonoBehaviour {
+
+	private static string LEFT_WALL = "LeftWall";
+	private static string RIGHT_WALL = "RightWall";
+
+	[SerializeField] GameObject ball;
+
+	[SerializeField] GameObject leftWall;
+	[SerializeField] GameObject rightWall;
+
+	[SerializeField] float moveSpeed;
+	[SerializeField] bool isMoving;
+	Vector3 direction = Vector3.right;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			ball.GetComponent<Rigidbody> ().isKinematic = false;	
+			isMoving = false;
+		}
+
+		MoveCrane ();
+
+	}
+
+	void MoveCrane(){
+
+		if(isMoving)
+			transform.Translate (direction * moveSpeed * Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log (col.name);
+
+		if (!isMoving)
+			return;
+		
+		if (col.name == LEFT_WALL) {
+			direction = Vector3.right;
+		} else if (col.name == RIGHT_WALL) {
+			direction = Vector3.left;
+		}
+	}
+}
